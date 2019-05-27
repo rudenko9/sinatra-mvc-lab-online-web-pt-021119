1cd1f974bfc79a92a53134f8660cafe792f0f79c
Nataliya R <n.rudenko@gmail.com>
2019-05-27 23:42:04 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -27,14 +27,6 @@ class PigLatinizer
       piglatinize(word)
     end.join(" ")
   end
-def piglatin(word)
-   if word =~ (/\A[aeiou]/i)
-      word = word + 'ay'
-   elsif word =~ (/\A[^aeiou]/i)
-      match = /\A[^aeiou]/i.match(word)
-      word = match.post_match + match.to_s + 'ay'
-   end
-word
-end
+
   
 end 
