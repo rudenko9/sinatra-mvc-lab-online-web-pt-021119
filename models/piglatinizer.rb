d83ca24e804d4804c39b656976771a56e2669452
Nataliya R <n.rudenko@gmail.com>
2019-05-27 23:58:08 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,37 +1,3 @@
 class PigLatinizer 
   
-  def piglatinize(phrase)
-    consonants = "bcdfghjklmnpqrstvwxyz"
-    consonants = consonants + consonants.upcase
-    vowels = "aeoui"
-    vowels = vowels + vowels.upcase
-    words = string.split
-    words.map do |word|
-      if consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
-        prefix = word.slice!(0..2)
-        word << prefix + "ay"
-      elsif consonants.include?(word[0]) && consonants.include?(word[1])
-        prefix = word.slice!(0..1)
-        word << prefix + "ay"
-      elsif consonants.include?(word[0])
-        prefix = word.slice!(0)
-        word << prefix + "ay"
-      elsif vowels.include?(word[0]) && word.size > 1
-        word << "way"
-      elsif vowels.include?(word[0]) && word.size == 1
-        word << "way"
-      end
-    end
-    words.join(" ")
-  end
-
-  
- 
-  def to_pig_latin(phrase)
-    phrase.split(" ").map do |word|
-      piglatinize(word)
-    end.join(" ")
-  end
-
-  
 end 
