889a87d4495986f43f89656679630846f01504fc
Nataliya R <n.rudenko@gmail.com>
2019-05-27 23:45:36 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,27 +1,5 @@
 class PigLatinizer 
-  def piglatinize(word)
-    @array = word.split("")
-    if "aeiouAEIOU".include?(@array[0])
-      word + "way"
-    else
-      @letter = ""
-      start_with_consonants
-    end
-  end
-
-  def start_with_consonants
-    @letter = @letter + @array.shift
-    if @array[0]
-      if "aeiouAEIOU".include?(@array[0])
-        @array.join + @letter + "ay"
-      else
-        start_with_consonants
-      end
-    else
-      ""
-    end
-  end
-
+ 
   def to_pig_latin(phrase)
     phrase.split(" ").map do |word|
       piglatinize(word)
