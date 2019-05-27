5da2f8320cd79348eb55e6ab17e1c26abce2da81
Nataliya R <n.rudenko@gmail.com>
2019-05-27 23:54:50 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,7 +1,29 @@
 class PigLatinizer 
   
   def piglatinize(phrase)
-    
+    consonants = "bcdfghjklmnpqrstvwxyz"
+    consonants = consonants + consonants.upcase
+    vowels = "aeoui"
+    vowels = vowels + vowels.upcase
+    words = string.split
+    words.map do |word|
+      if consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
+        prefix = word.slice!(0..2)
+        word << prefix + "ay"
+      elsif consonants.include?(word[0]) && consonants.include?(word[1])
+        prefix = word.slice!(0..1)
+        word << prefix + "ay"
+      elsif consonants.include?(word[0])
+        prefix = word.slice!(0)
+        word << prefix + "ay"
+      elsif vowels.include?(word[0]) && word.size > 1
+        word << "way"
+      elsif vowels.include?(word[0]) && word.size == 1
+        word << "way"
+      end
+    end
+    words.join(" ")
+  end
 
   
  
