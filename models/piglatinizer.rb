3f0cacce70ce30964c682ac3ae6909f5d3bbd6b9
Nataliya R <n.rudenko@gmail.com>
2019-05-27 23:54:37 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,6 +1,6 @@
 class PigLatinizer 
   
-  def piglatinize(word)
+  def piglatinize(phrase)
     consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"]
     consonants = consonants + consonants.upcase
     vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
