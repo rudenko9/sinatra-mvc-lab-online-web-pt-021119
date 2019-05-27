f3042f80e6d627ab5661672cc81ab2a1b1d62a1a
Nataliya R <n.rudenko@gmail.com>
2019-05-27 23:09:23 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,16 +1,6 @@
 class PigLatinizer 
   attr_accessor :text 
-  
-  @@all = []
-  
-  #def initialize(text)
-   # @text = text.downcase 
-    #@@all << self
-   # end 
-  
-  def self.new 
-    @@all 
-  end
+ 
   def cut_front 
     
     
